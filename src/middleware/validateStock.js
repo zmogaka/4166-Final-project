7b6b46ffa35d9cc0d8a053fb7b559559aac2ce52
tweber13@charlotte.edu <tweber13@charlotte.edu>
2025-11-20 20:36:52 -0500

    Update authentication to include sector

--- src/middleware/validateStock.js
+++ src/middleware/validateStock.js
@@ -1,5 +1,5 @@
 import { body } from "express-validator";
-import { handleValidationErrors } from "./handleValidationErrors.js";
+import { checkValidationResults } from "./handleValidationErrors.js";
 
 export const validateStock = [
   body("symbol")
@@ -12,22 +12,23 @@ export const validateStock = [
     .isLength({ min: 1, max: 10 })
     .withMessage("symbol must be between 1 and 10 characters"),
 
-  body("companyName")
+  body("name")
     .exists({ checkFalsy: true })
-    .withMessage("companyName is required")
+    .withMessage("name is required")
     .bail()
     .isString()
-    .withMessage("companyName must be a string")
+    .withMessage("name must be a string")
     .bail()
     .isLength({ min: 2, max: 100 })
-    .withMessage("companyName must be between 2 and 100 characters"),
+    .withMessage("name must be between 2 and 100 characters"),
 
-  body("currentPrice")
-    .exists({ checkFalsy: true })
-    .withMessage("currentPrice is required")
+  body("sector")
+    .optional()
+    .isString()
+    .withMessage("sector must be a string")
     .bail()
-    .isFloat({ gt: 0 })
-    .withMessage("currentPrice must be a positive number"),
+    .isLength({ min: 2, max: 100 })
+    .withMessage("sector must be between 2 and 100 characters"),
 
-  handleValidationErrors,
+  checkValidationResults,
 ];
